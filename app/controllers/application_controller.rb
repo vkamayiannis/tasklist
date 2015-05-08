609117afeaead09bcd20f28ab22c6f0b77d4f819
Valantis Kamayiannis <valantis@orosimo.com.gr>
2015-05-08 17:25:50 +0300

    fixed i18n on event index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,19 +2,21 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   before_filter :set_locale
-  before_filter :set_user_language
+  #before_filter :set_user_language
 
   def set_locale
   	I18n.locale = params[:locale] || I18n.default_locale
   end
 
+  # def set_user_language
+  #   #I18n.locale = 'en'
+  #   I18n.locale = params[:locale] || I18n.default_locale
+  # end
+
   protect_from_forgery with: :exception
 
   def default_url_options(options={})
   	{locate: I18n.locale}.merge options
-  end
-  def set_user_language
-    I18n.locale = 'gr'
   end
 
 end
